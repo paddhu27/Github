d7d30789e73cbea4642e9716c54569cf7fbaad55
padma k <101664110+paddhu27@users.noreply.github.com>
2022-03-17 07:14:42 +0530

    Add files via upload

---
+++ calculator python1.py
@@ -0,0 +1,31 @@
+from ast import Yield
+def addition(x,y):
+	result=x+y
+	print(x,"+",y,"=",result)
+def subtraction(x,y):
+	result=x-y
+	print(x,"-",y,"=",result)
+def multiplication(x,y):
+	result=x*y
+	print(x,"*",y,"=",result)
+def division(x,y):
+	result=x/y
+	print(x,"/",y,"=",result)
+number1=float(input("enter first number:"))
+number2=float(input("enter second number:"))
+print("choose the operation that you want to perform")
+print("+ for addition")
+print("- for Subtraction")
+print("* for Multiplication")
+print("/ for Divison")
+choice=input()
+if (choice == '+'):
+	addition(number1,number2)
+elif (choice == '-'):
+	subtraction(number1,number2)
+elif (choice == '*'):
+	multiplication(number1,number2)
+elif (choice == '/'):
+	division(number1,number2)
+else:
+	print("invalid entry")
